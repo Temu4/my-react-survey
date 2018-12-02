61ace9cff59cc4259899a9e1b369cfcbcc732880
Артем <42999342+Temu4@users.noreply.github.com>
2018-12-03 02:51:41 +0300

    deleted redundant files

---
+++ src/Button.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Button extends React.Component {
+  constuctor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return <button />;
+  }
+}
+
+export default Button;
