e1bcc12d220d0314dfdf7aa3dabc3fa6929d8ae9
Артем <42999342+Temu4@users.noreply.github.com>
2018-12-11 00:04:22 +0300

    Button.js
    hide Button component

--- src/Button.js
+++ src/Button.js
@@ -1,15 +1,15 @@
-import React from "react";
+// import React from "react";
 
-class Button extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-  render() {
-    var answers = this.props.answerArray.map(item => {
-      return <button onClick>Answer1: {item.answer}</button>;
-    });
-    return <div>{answers}</div>;
-  }
-}
+// class Button extends React.Component {
+//   constructor(props) {
+//     super(props);
+//   }
+//   render() {
+//     var answers = this.props.answerArray.map(item => {
+//       return <button onClick>Answer1: {item}</button>;
+//     });
+//     return <div>{answers}</div>;
+//   }
+// }
 
-export default Button;
+// export default Button;
